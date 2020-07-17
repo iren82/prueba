7b6a4330af657f482326a64b678167034f0e0dd0
Nico Fleming <nicofleming82@gmail.com>
2020-07-17 09:02:29 -0300

    Commiteando archivos JS y HTML

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//********************************
+//*** Obteniendo datos con fetch
+
+var boton = document.getElementById('boton');
+var contenedor = document.getElementById('contenedor');
+var posts = null;
+
+boton.addEventListener('click', function () {
+
+    fetch('http://jsonplaceholder.typicode.com/posts') // Trae los datos
+    .then(data => data.json()) // Trabaja con los datos
+    .then(data => {
+        posts = data;
+        mostrarDatos(posts)
+    })
+
+});
+
+function mostrarDatos(posts) {
+    posts.map((post, i) => {
+        let titulo = document.createElement('h1');
+        let contenido = document.createElement('p');
+
+        titulo.innerHTML = (i + 1) + " - " + post.title;
+        contenido.innerHTML = post.body;
+
+        contenedor.appendChild(titulo);
+        contenedor.appendChild(contenido);
+    })
+}
