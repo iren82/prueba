d99a00811d55a81f32f46faf1b5083eb9fb2bc10
Nico Fleming <nicofleming82@gmail.com>
2020-07-17 09:04:43 -0300

    Cambio en app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ boton.addEventListener('click', function () {
         mostrarDatos(posts)
     })
 
-});
+}); // Agregando comment para testear el GIT
 
 function mostrarDatos(posts) {
     posts.map((post, i) => {
